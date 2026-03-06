c5b30aba9e88d313ffe8d6b5d44b902b2fcd2da0
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-06 10:09:18 +0530

    refactor: complete ec2 security group module

--- modules/ec2_sg/main.tf
+++ modules/ec2_sg/main.tf
@@ -1,3 +1,7 @@
+data "aws_ec2_managed_prefix_list" "cloudfront" {
+  name = "com.amazonaws.global.cloudfront.origin-facing"
+}
+
 resource "aws_security_group" "server_sg" {
   name = var.sg_name
   description = "allow HTTP,HTTPS for alb sg and SSH for my ip"
@@ -7,8 +11,45 @@ resource "aws_security_group" "server_sg" {
   }
 }
 
-resource "aws_security_group_rule" "allow_" {
-  
+resource "aws_security_group_rule" "allow_ssh" {
+  type = "ingress"
+  description = "SSH ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["220.247.240.217/32"]
+  security_group_id = aws_security_group.server_sg.id
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  type = "ingress"
+  description = "Allow HTTP traffic from cloudfront"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront.id]
+  security_group_id = aws_security_group.server_sg.id
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  type = "ingress"
+  description = "Allow HTTPS traffic from cloudfront"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  prefix_list_ids = [ data.aws_ec2_managed_prefix_list.cloudfront.id ]
+  security_group_id = aws_security_group.server_sg.id
+}
+
+# outbound rules
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  description       = "Allow all outbound traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.server_sg.id
 }
 
 
