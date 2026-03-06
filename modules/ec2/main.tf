fa110e2d3a913be4b30182b8209c872c09cf4064
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-06 12:36:02 +0530

    checkov: instance metadata service version issie

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -29,6 +29,16 @@ resource "aws_instance" "server_instance" {
 
   key_name = var.key_pair_name
 
+  # Enforces Instance Metadata Service Version 2 (IMDSv2) to mitigate SSRF-based credential theft attacks.
+  # IMDSv1 allows any process on the instance to query metadata via a simple HTTP GET request without authentication.
+  # IMDSv2 requires a session-oriented token, which must be obtained via a PUT request before accessing metadata.
+  # Reference: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/configuring-instance-metadata-service.html
+  metadata_options {
+    http_endpoint               = "enabled"  # Enables the Instance Metadata Service (IMDS) endpoint on the instance
+    http_tokens                 = "required" # Enforces IMDSv2 by requiring a session token; disables IMDSv1 completely
+    http_put_response_hop_limit = 1          # Restricts metadata access to the instance itself; prevents containerized workloads from reaching the host metadata service
+  }
+
   tags = {
     Name = var.instance_name
   }
