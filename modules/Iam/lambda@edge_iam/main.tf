13a0f863ad4bbf664db90837e0de1a84a317cbd9
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-07 19:49:19 +0530

    fix(iam): attach AWSLambdaBasicExecutionRole and add Kinesis PutRecord for Lambda@Edge role

--- modules/Iam/lambda@edge_iam/main.tf
+++ modules/Iam/lambda@edge_iam/main.tf
@@ -1,18 +1,3 @@
-data "aws_iam_policy_document" "lambda_policy_document" {
-    version = "2012-10-17"
-
-    statement {
-      effect = "Allow"
-      actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = ["arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:*"]
-    }
-}
-
 data "aws_iam_policy_document" "assume_role_policy" {
     statement {
       effect = "Allow"
@@ -24,18 +9,33 @@ data "aws_iam_policy_document" "assume_role_policy" {
     }
 }
 
+data "aws_iam_policy_document" "lambda_kinesis_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["kinesis:PutRecord"]
+    resources = ["arn:aws:kinesis:*:${data.aws_caller_identity.current.account_id}:stream/*"]
+  }
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = var.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name = "${var.role_name}-lambda_policy"
-  description = "Iam policy for lambda edge"
-  policy = data.aws_iam_policy_document.lambda_policy_document.json
+resource "aws_iam_policy" "lambda_kinesis_policy" {
+  name        = "${var.role_name}-kinesis-policy"
+  description = "Kinesis PutRecord permission for Lambda@Edge"
+  policy      = data.aws_iam_policy_document.lambda_kinesis_policy_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_role_policy_attach" {
-  role = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+# Attach defult the AWS-managed AWSLambdaBasicExecutionRole
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Attach the custom Kinesis policy
+resource "aws_iam_role_policy_attachment" "lambda_kinesis_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_kinesis_policy.arn
 }
