2966123eb51002dae02adcadac375a848fa9c177
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-08 12:39:19 +0530

    feat: complete kinesis data stream module

---
+++ modules/kinesis_data_stream/main.tf
@@ -0,0 +1,17 @@
+resource "aws_kinesis_stream" "this" {
+  name = var.stream_name
+  retention_period = var.retention_period_hours
+
+  stream_mode_details {
+    stream_mode = "ON_DEMAND"
+  }
+
+  # Encrypt records at rest using the AWS-managed Kinesis key
+  encryption_type = "KMS"
+  kms_key_id      = "alias/aws/kinesis"
+
+  tags = merge(var.tags, {
+    Module = "kinesis_stream"
+    Purpose = "edge-analytics-ingest"
+  })
+}
---
+++ modules/kinesis_data_stream/variables.tf
@@ -0,0 +1,15 @@
+variable "stream_name" {
+  type = string
+  description = "kinesis data stream name" # "cloudfront-edge-events"
+}
+
+variable "retention_period_hours" {
+  type = number
+  description = "records are kept in the stream before expiring"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
