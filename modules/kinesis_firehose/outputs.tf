acbe3fa26b17ff37d071af4035be2760ce897f88
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-10 15:32:23 +0530

    fix: remove unnecessary output

--- modules/kinesis_firehose/outputs.tf
+++ modules/kinesis_firehose/outputs.tf
@@ -7,8 +7,3 @@ output "firehose_name" {
   description = "Name of the Kinesis Firehose delivery stream."
   value       = aws_kinesis_firehose_delivery_stream.this.name
 }
-
-output "firehose_role_arn" {
-  description = "IAM execution role ARN used by Firehose"
-  value       = aws_iam_role.firehose.arn
-}
