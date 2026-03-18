89f936a67f08a775b9256197efe8a7ffe1b78c8d
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-18 12:30:42 +0530

    fix : add required provider and versions

--- terraform.tf
+++ terraform.tf
@@ -7,4 +7,13 @@ terraform {
       name = "aws-serverless-edge-request-analytics-via-kinesis-to-datadog"
     }
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
 }
