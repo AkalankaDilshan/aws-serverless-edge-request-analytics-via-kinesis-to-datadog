3c2d48816e7e7d681ac419a848cb73c71d2edca6
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-03-18 12:33:53 +0530

    feat: add more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "ec2_public_dns" {
   value = module.ec2_server.instance_public_dns
 }
+
+output "cloudfront_domain_name" {
+  value = module.cloudfront.cdn_domain_name
+}
+
+output "kinesis_stream_arn" {
+  value = module.kinesis_stream.stream_arn
+}
+
+output "lambda_edge_arn" {
+  value = module.lambdaedge_function.lambda_qualified_arn
+}
